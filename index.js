6622198e674f54b1788458f56a58d1e223c44985
Rafael plaza Daza <rafaelplazadaza@gmail.com>
2024-09-11 17:25:13 -0500

    v1-version

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 const { exec, execSync } = require("child_process");
 const { promisify } = require("util");
 var player = require("play-sound")((opts = {}));
@@ -7,8 +9,12 @@ const { stdout } = require("process");
 const execAsync = promisify(exec);
 // const local
 const DURANTIONSOUND = 40000;
+const TIMESLEEP = 300000;
+const MINBACTERYLEVEL = 80;
+const CHARGING = "charging";
+const DISCHARGING = "discharging";
 let counter = 0;
-const music = "./media/alarm.mp3";
+const music = "/home/rafael/workspace/alert-bactery/media/alarm.mp3";
 
 function execPlaySoun() {
   return player.play(music, (err) => {
@@ -29,24 +35,16 @@ function getInfoBatery() {
   }
 }
 
-async function syncFuntion() {
+async function main() {
   try {
     const wait = (t) =>
       new Promise((resolve, reject) => setTimeout(resolve, t));
 
     do {
       let info = getInfoBatery();
-      //  get list of sounds
-      // const songs = fs
-      //   .readdirSync(music)
-      //   .filter((file) => path.extname(file).toLowerCase() === ".mp3");
 
-      if (info.percentage < 82 && info.state === "discharging") {
-        // const songPath = path.join(music, songs[0]);
-        // console.log(`Reproduciendo: ${songs[1]}`);
-
+      if (info.percentage < MINBACTERYLEVEL && info.state === DISCHARGING) {
         let audio = execPlaySoun();
-        //check is chargin
 
         let isNotCharging = true;
 
@@ -56,38 +54,23 @@ async function syncFuntion() {
           if (counter >= DURANTIONSOUND) {
             audio.kill();
             audio = execPlaySoun();
-            console.log("restar music");
             counter = 0;
           }
 
-          if (info.state === "charging") {
+          if (info.state === CHARGING) {
             isNotCharging = false;
             audio.kill();
           }
 
-          console.log("into loop intern");
           await wait(5000);
           counter += 5000;
         } while (isNotCharging);
       }
 
-      await wait(60000);
-
-      console.log(`afther ${JSON.stringify(info)}`);
+      await wait(TIMESLEEP);
     } while (true);
   } catch (err) {
     console.error(`Error ${err.toString()}`);
-  }
-}
-async function execShowInfo() {
-  try {
-    const { stdout } = await execAsync(
-      'upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|percentage"',
-    );
-    return filterInformation(stdout);
-  } catch (error) {
-    console.error("Error executing command:", error);
-    return null;
   }
 }
 
@@ -110,47 +93,4 @@ function filterInformation(text) {
   return info;
 }
 
-async function checkBatteryStatus(threshold) {
-  const batteryInfo = await execShowInfo();
-  if (batteryInfo && batteryInfo.percentage > threshold) {
-    console.log(`¡Alerta! Batería baja: ${batteryInfo.percentage}%`);
-    // Aquí puedes agregar el código para emitir un sonido
-  }
-}
-
-// Ejemplo de uso
-const BATTERY_THRESHOLD = 20;
-const CHECK_INTERVAL = 60000; // 1 minuto
-
-//setInterval(() => {
-//  checkBatteryStatus(BATTERY_THRESHOLD);
-//}, CHECK_INTERVAL);
-//
-//// Ejecutar inmediatamente al inicio
-//checkBatteryStatus(BATTERY_THRESHOLD);
-let testFunction = async () => {
-  do {
-    const info = await execShowInfo();
-    const music = "./media";
-    console.log("other ejecucion");
-    //  Obtener lista de canciones
-    const songs = fs
-      .readdirSync(music)
-      .filter((file) => path.extname(file).toLowerCase() === ".mp3");
-    console.log("before while");
-    if (info.percentage > 20 && info.state === "discharging") {
-      const songPath = path.join(music, songs[0]);
-      console.log(`Reproduciendo: ${songs[1]}`);
-
-      player.play(songPath, (err) => {
-        if (err) console.log(`Error al reproducir: ${err}`);
-      });
-    }
-    console.log("ejecucion interna");
-    //
-    //
-    // console.log(`Info : ${batteryInfo.percentage}`)
-  } while (true);
-};
-//testFunction();
-syncFuntion();
+main();
